1b9b5f037e066f95ca89a1cd9260636018c0830b
ptkngan7 <ptkngan7@gmail.com>
2021-10-15 14:46:17 +0700

    fc

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/lib',express.static(path.join(__dirname, 'public/lib')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
